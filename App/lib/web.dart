8992d51a95dfebec4e3b7e9dea58150eb1bf5b64
hm-software <daxionginfo@gmail.com>
2020-02-21 17:40:53 +0700

    fixed

--- App/lib/web.dart
+++ App/lib/web.dart
@@ -1,20 +1,76 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:http/http.dart' as http;
+
 class Web extends StatefulWidget {
   @override
   _WebState createState() => _WebState();
 }
 
 class _WebState extends State<Web> {
+  var url='http://192.168.0.102';
+  bool loading = true;
+  bool hostyes = true;
+
+  void checkingIp() async {
+    try {
+      final response = await http.get(url);
+      setState(() {
+        loading = false;
+      });
+    } on Exception catch (_) {
+      setState(() {
+        loading=true;
+        hostyes = false;
+      });
+    }
+  }
+
   @override
+  void initState() {
+    super.initState();
+    checkingIp();
+  }
+
   Widget build(BuildContext context) {
-    return WebviewScaffold(
-          url: "http://192.168.100.167:8080",
-          withJavascript: true,
-          withLocalStorage: true,
-          resizeToAvoidBottomInset : true,
-          
-    );
+    return loading
+        ? Material(
+            color: Colors.white,
+            child: Center(
+              child: hostyes
+                  ? CircularProgressIndicator()
+                  : Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Text(
+                          'ເຊື່ອມຕໍ່ຜິດພາດ/Error connection.!',
+                          style: TextStyle(fontSize: 12.0, color: Colors.brown[200]),
+                        ),
+                        Text(''),
+                        Ink(
+                          decoration: const ShapeDecoration(
+                            color: Colors.lightBlue,
+                            shape: CircleBorder(),
+                          ),
+                          child: IconButton(
+                            icon: Icon(Icons.refresh),
+                            color: Colors.white,
+                            onPressed: () {
+                              Navigator.pushReplacement(context,
+                                  MaterialPageRoute(builder: (_) => Web()));
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+            ),
+          )
+        : WebviewScaffold(
+            url:url,
+            withJavascript: true,
+            withLocalStorage: true,
+            resizeToAvoidBottomInset: true,
+          );
   }
 }
